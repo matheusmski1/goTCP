89dc39daed0a79adb2e04871a011b02a9a9c392f
antoniosgarbi <antonio.sgarbi@hotmail.com>
2022-09-29 19:23:20 -0300

    biblioteca grafica adicionada

---
+++ model/struct.go
@@ -0,0 +1,8 @@
+package model
+
+type Pessoa struct {
+	Nome     string
+	Email    string
+	Telefone string
+	Idade    int64
+}
--- process/server.go
+++ process/server.go
@@ -1,38 +1,21 @@
-package server
+package process
 
 import (
 	"bufio"
 	"fmt"
 	"net"
-	"os"
 	"strings"
 	"time"
 )
 
-func RunTcpClientConction() {
-	c, err := net.Dial("tcp", "1234")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+func Server() {
+	//arguments := os.Args
+	//if len(arguments) == 1 {
+	//	fmt.Println("Please provide port number")
+	//	return
+	//}
 
-	for {
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print(">> Digite algo")
-		text, _ := reader.ReadString('\n')
-		fmt.Fprintf(c, text+"\n")
-
-		message, _ := bufio.NewReader(c).ReadString('\n')
-		fmt.Print("->: " + message)
-		if strings.TrimSpace(string(text)) == "STOP" {
-			fmt.Println("TCP client desligando...")
-			return
-		}
-	}
-}
-
-func RunTcpServer() {
-	PORT := ":1234"
+	PORT := ":" + "1290" //arguments[1]
 	l, err := net.Listen("tcp", PORT)
 	if err != nil {
 		fmt.Println(err)
@@ -57,6 +40,7 @@ func RunTcpServer() {
 			return
 		}
 
+		fmt.Print("-> ", string(netData))
 		t := time.Now()
 		myTime := t.Format(time.RFC3339) + "\n"
 		c.Write([]byte(myTime))
